31dfe021bbabb2c229b97fb164e173e99800f960
mits <devanshu18g@gmail.com>
2021-05-08 20:01:27 +0530

    Completed & Tested

--- app/src/main/java/com/example/readjson/MainActivity.java
+++ app/src/main/java/com/example/readjson/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.readjson;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    String name,text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ReadConfig readConfig = new ReadConfig();
+        final JSONObject jsonObject = readConfig.loadJSONFromAsset(this,"config.json");
+        try {
+            if(jsonObject !=null){
+                name = jsonObject.getString("name");
+                text = jsonObject.getString("text");
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        TextView txt = findViewById(R.id.textView);
+        txt.setText("Name : " + name + "\nText :" + text);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hello !", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
